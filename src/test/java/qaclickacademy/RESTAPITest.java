3a74c43f33bc0bbba54a0dae8e7bb5c29bd50604
Zasidul <Zasidulislam7@gmail.com>
2021-01-13 14:59:57 -0500

    Newest commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -1,44 +1,42 @@
 package qaclickacademy;
 
 import org.testng.annotations.Test;
+
 //rahulonlinetutor@gmail.com
 public class RESTAPITest {
 
 	@Test
-	public void postJira()
-	{
+	public void postJira() {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
-		
-		
+
 		System.out.println("Hello1");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
-		
-		
-		
+
 		System.out.println("Hello1");
 		System.out.println("Hello2");
-		System.out.println("Hello3");System.out.println("Hello1");
+		System.out.println("Hello3");
+
+		System.out.println("Hello1");
 		System.out.println("Hello2");
-		System.out.println("Hello3");System.out.println("Hello1");
+		System.out.println("Hello3");
+
+		System.out.println("Hello1");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
 	}
-	
+
 	@Test
-	public void deleteTwitter()
-	{
+	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
-		
-		
+
 	}
+
 	@Test
-	public void deleteTwitter12()
-	{
+	public void deleteTwitter12() {
 		System.out.println("deleteTwddtter");
-		
-		
+
 	}
 }
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -4,30 +4,30 @@ import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-	//testng xml file from Maven
-	// how to execute all test cases from Test folder  - mvn test
+	// testng xml file from Maven
+	// how to execute all test cases from Test folder - mvn test
 	@Test
-	public void BrowserAUtomation()
-	{
-		System.out.println("BrowserAUtomation");	
-		
-		System.out.println("Web automation");	
+	public void BrowserAUtomation() {
+		System.out.println("BrowserAUtomation");
+
+		System.out.println("WebAutomation");
+
+		System.out.println("Web automation");
 	}
+
 	@Test
-	public void elementsUi()
-	{
-		System.out.println("elementsUi");	
+	public void elementsUi() {
+		System.out.println("elementsUi");
 	}
+
 	@Test
-	public void elementsU1i()
-	{
-		System.out.println("elementfdsUi");	
+	public void elementsU1i() {
+		System.out.println("elementfdsUi");
 	}
+
 	@Test
-	public void elementsU1ssi()
-	{
-		System.out.println("elementfdsUi");	
+	public void elementsU1ssi() {
+		System.out.println("elementfdsUi");
 	}
-	
-	
+
 }
